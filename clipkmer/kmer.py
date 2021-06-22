7654343fe036941acced415797a8a907a733a4a2
Jiayi-Wang-Joey <591813079@qq.com>
2021-06-22 02:52:58 +0100

    first commit

--- clipkmer/kmer.py
+++ clipkmer/kmer.py
@@ -1,2 +1,20 @@
-for i in range(0,101):
-    print(i**2)
+# define a function to define the frequency of a certain length of fragment in a DNA sequence 
+def kmer_frequency():
+    filename = input("Please enter your txt file name (including directory): ")
+    # define the kmer length
+    length = int(input("Please enter kmer length: "))
+    with open(filename,'r') as file:
+        sequence = file.read()
+        sequence = sequence.replace("\n","")
+        output = {}
+        for i in range(0,len(sequence)-length+1):
+            fragment = sequence[i:i+length]
+            count = sequence.count(fragment)
+            output[fragment] = count
+        print(output)
+        
+
+
+
+if __name__ == '__main__':
+    kmer_frequency()
