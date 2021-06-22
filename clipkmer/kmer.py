8019edfaacda95f67f6045a1f3d123837f819045
Jiayi-Wang-Joey <591813079@qq.com>
2021-06-22 16:31:45 +0100

    first commit

--- clipkmer/kmer.py
+++ clipkmer/kmer.py
@@ -1,20 +1,24 @@
 # define a function to define the frequency of a certain length of fragment in a DNA sequence 
-def kmer_frequency():
-    filename = input("Please enter your sequence file (including directory): ")
-    # define the kmer length
-    length = int(input("Please enter kmer length: "))
+def read_sequence(filename):
     with open(filename,'r') as file:
         sequence = file.read()
         sequence = sequence.replace("\n","")
-        output = {}
-        for i in range(0,len(sequence)-length+1):
-            fragment = sequence[i:i+length]
-            count = sequence.count(fragment)
-            output[fragment] = count
-        print(output)
+    return sequence
+
+def kmer_frequency(sequence,length):
+    output = {}
+    for i in range(0,len(sequence)-length+1):
+        fragment = sequence[i:i+length]
+        count = sequence.count(fragment)
+        output[fragment] = count
+    print(output)
+    return output
         
 
 
 
 if __name__ == '__main__':
-    kmer_frequency()
+    filename = input("Enter the file directory: ")
+    sequence = read_sequence(filename)
+    length = int(input("Enter the kmer length: "))
+    kmer_frequency(sequence,length)
--- clipkmer/test.py
+++ clipkmer/test.py
@@ -1,22 +1,12 @@
 import unittest
-def kmer_frequency(sequence = str, length = int):
-    frequence = []
-    for i in range(0,len(sequence)-length+1):
-        fragment = sequence[i:i+length]
-        count = sequence.count(fragment)
-        frequence.append(count)
-    max_frequence = max(frequence)
-    return max_frequence
-
-test_sequence1 = "ATCGATCGATCGATCGATCGATCG" # kmer sequence 4, max frequence = 6
-test_sequence2 = "AAAAAAAAAA" # kmer sequence 1, max frequence = 10
-test_sequence3 = "ATCCGATCCGATCCG" # kmer sequence 5, max frequence = 3
+from kmer import read_sequence, kmer_frequency
+test_sequence1 = read_sequence("/Users/apple/Desktop/file1.txt") #AAAAA, kmer length = 1, {'A':5}
+test_sequence2 = read_sequence("/Users/apple/Desktop/file2.txt") #ATCGATCG, kmer length = 4, {'ATCG':2, 'TCGA':1, 'CGAT':1, 'GATC': 1}
 
 class test(unittest.TestCase):
-    def test_kmer_frequency(self):
-        self.assertTrue(kmer_frequency(test_sequence1,4) == 6)
-        self.assertTrue(kmer_frequency(test_sequence2,1) == 10)
-        self.assertTrue(kmer_frequency(test_sequence3,5) == 3)
-    
+    def test_kmer_sequence(self):
+        self.assertTrue(kmer_frequency(test_sequence1,1)) == {'A': 5}
+        self.assertTrue(kmer_frequency(test_sequence2,4)) == {'ATCG': 2, 'TCGA': 1, 'CGAT': 1, 'GATC': 1}
+
 if __name__ == "__main__":
     unittest.main()
